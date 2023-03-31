35e04e1232695b90c2fb54bf2be8d6dbf262353b
ApostL78 <vgwrbrvw@mail.ru>
2023-03-31 17:42:05 +0400

    fix title

--- orgs_api/yasg.py
+++ orgs_api/yasg.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Django Movie",
+        title="Orgs API",
         default_version="v1",
         description="Test description",
         license=openapi.License(name="BSD License"),
